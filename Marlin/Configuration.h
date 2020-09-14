fc1374ac2dfbc4e37ec6355660c6120cb8e5bde7
Colton Nicotera <Colton Nicotera@COLTON-PC.NICOTERA.local>
2020-09-14 18:04:20 -0400

    Change bed size to 400x400x500

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1137,8 +1137,8 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 235
-#define Y_BED_SIZE 235
+#define X_BED_SIZE 400
+#define Y_BED_SIZE 400
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
@@ -1146,7 +1146,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 250
+#define Z_MAX_POS 500
 
 /**
  * Software Endstops
